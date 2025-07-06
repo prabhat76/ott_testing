8cc32d4037ed9516286ee124379e0966838c990a
Prabhat kumar <prabhatkumar@Prabhats-MacBook-Pro.local>
2025-07-06 15:12:25 +0530

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'financial.services',
+  appName: 'financial-services',
+  webDir: 'dist'
+};
+
+export default config;
